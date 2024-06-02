2a21efb035ed3998f9deccd7a6c63198366c0577
Chukwuemeka Augustine Anoliefo <45601527+ChukwumekaAugustineAnoliefo@users.noreply.github.com>
2024-06-02 03:41:11 -0700

    BizcardApp Avatar created

--- bizcardapp/lib/ui/home.dart
+++ bizcardapp/lib/ui/home.dart
@@ -3,29 +3,11 @@ import 'package:flutter/material.dart';
 class BizCardApp extends StatelessWidget {
   const BizCardApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Bizcard"),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Text('Hello'),
-            _getCard(),
-          ],
-        ),
-      ),
-    );
-  }
-
   Container _getCard() {
     return Container(
       width: 350,
       height: 200,
+      margin: const EdgeInsets.all(50),
       decoration: BoxDecoration(
         color: Colors.pinkAccent,
         borderRadius: BorderRadius.circular(15),
@@ -33,8 +15,17 @@ class BizCardApp extends StatelessWidget {
       child: const Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('TechieChuks Flutter'),
-          Text('com.github/ChukwuemekaAA'),
+          Text(
+            'TechieChuks Flutter',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            'com.github/ChukwuemekaAA',
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -47,6 +38,53 @@ class BizCardApp extends StatelessWidget {
             ],
           )
         ],
+      ),
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(50.0),
+          ),
+          border: Border.all(
+            color: Colors.redAccent,
+            width: 1.2,
+          ),
+          image: const DecorationImage(
+            image: NetworkImage(
+              'https://miro.medium.com/max/1200/0*TMrBkQSx9t87IxkC',
+            ),
+            fit: BoxFit.cover,
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Bizcard",
+        ),
+      ),
+      backgroundColor: Colors.pinkAccent,
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            const Text(
+              'Hello',
+            ),
+            _getCard(),
+            _getAvatar(),
+          ],
+        ),
       ),
     );
   }
