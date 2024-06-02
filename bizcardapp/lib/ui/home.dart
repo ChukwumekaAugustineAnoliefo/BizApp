09c6febe636eac0d605ddda76cff2d0789baaa4d
Chukwuemeka Augustine Anoliefo <45601527+ChukwumekaAugustineAnoliefo@users.noreply.github.com>
2024-06-02 03:07:32 -0700

    Card Widget Created

---
+++ bizcardapp/lib/ui/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class BizCardApp extends StatelessWidget {
+  const BizCardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bizcard"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Text('Hello'),
+            _getCard(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _getCard() {
+    return Container(
+      width: 350,
+      height: 200,
+      decoration: BoxDecoration(
+        color: Colors.pinkAccent,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('TechieChuks Flutter'),
+          Text('com.github/ChukwuemekaAA')
+        ],
+      ),
+    );
+  }
+}
